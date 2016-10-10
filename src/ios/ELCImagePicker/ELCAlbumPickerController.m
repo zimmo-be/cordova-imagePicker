7cd7bf3c8c147beed550635ad0d9c12ad265e02a
Bram Decuypere <bram.decuypere@gmail.com>
2016-10-10 10:28:15 +0200

    update error message

--- src/ios/ELCImagePicker/ELCAlbumPickerController.m
+++ src/ios/ELCImagePicker/ELCAlbumPickerController.m
@@ -67,7 +67,7 @@
             // Group Enumerator Failure Block
             void (^assetGroupEnumberatorFailure)(NSError *) = ^(NSError *error) {
                 
-                UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"Error" message:[NSString stringWithFormat:@"Album Error: %@ - %@", [error localizedDescription], [error localizedRecoverySuggestion]] delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+                UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"Autorisatiefout" message:[NSString stringWithFormat:@"Ga naar 'instellingen' van uw toestel om de nodige rechten (Foto's) toe te voegen aan de Max-Immo applicatie."] delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
                 [alert show];
                 
                 NSLog(@"A problem occured %@", [error description]);	                                 
